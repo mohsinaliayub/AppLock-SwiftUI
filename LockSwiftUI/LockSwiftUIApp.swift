b3a97dbd681e436ea5cb8b25429aac177860a5bb
Mohsin Ali Ayub <meismosin@gmail.com>
2023-10-20 12:12:33 +0200

    Initial Commit

---
+++ LockSwiftUI/LockSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockSwiftUIApp.swift
+//  LockSwiftUI
+//
+//  Created by Mohsin Ali Ayub on 20.10.23.
+//
+
+import SwiftUI
+
+@main
+struct LockSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
