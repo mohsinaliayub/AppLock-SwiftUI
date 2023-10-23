d069295cecbf4611d9a0a8f3cc28a8ced98de381
Mohsin Ali Ayub <meismosin@gmail.com>
2023-10-23 10:26:49 +0200

    number pad interface

--- LockSwiftUI/Views/LockView.swift
+++ LockSwiftUI/Views/LockView.swift
@@ -14,6 +14,11 @@ struct LockView<Content: View>: View {
     var isEnabled: Bool
     var lockWhenAppGoesBackground: Bool = true
     @ViewBuilder var content: Content
+    var forgotPin: () -> () = { }
+    
+    // State properties
+    @State private var pin: String = ""
+    
     
     var body: some View {
         GeometryReader {
@@ -24,15 +29,88 @@ struct LockView<Content: View>: View {
             
             if isEnabled {
                 ZStack {
+                    Rectangle().ignoresSafeArea()
+                    
                     if lockType == .both || lockType == .biometric {
-                        Rectangle()
+                        
                     } else {
                         // Custom number pad to view lock pin
+                        numberPadPinView()
                     }
                 }
-                .ignoresSafeArea()
             }
         }
+    }
+    
+    /// Numberpad Pin View.
+    @ViewBuilder
+    func numberPadPinView() -> some View {
+        VStack(spacing: 16) {
+            Text("Enter pin")
+                .font(.title.bold())
+                .frame(maxWidth: .infinity)
+            
+            HStack(spacing: 10) {
+                ForEach(0..<4, id: \.self) { index in
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 50, height: 55)
+                }
+            }
+            .padding(.top, 16)
+            .overlay(alignment: .bottomTrailing) {
+                Button("Forgot pin?", action: forgotPin)
+                    .foregroundStyle(.white)
+                    .offset(y: 40)
+            }
+            .frame(maxHeight: .infinity)
+            
+            // Custom number pad
+            GeometryReader { _ in
+                LazyVGrid(columns: Array(repeating: GridItem(), count: 3), content: {
+                    ForEach(1...9, id: \.self) { number in
+                        Button(action: { }) {
+                            Text("\(number)")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 20)
+                                .contentShape(.rect)
+                        }
+                        .tint(.white)
+                    }
+                    
+                    // 0 and back button
+                    Button(action: { }) {
+                        Text("")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 20)
+                            .contentShape(.rect)
+                    }
+                    .tint(.white)
+                    
+                    Button(action: { }) {
+                        Text("0")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 20)
+                            .contentShape(.rect)
+                    }
+                    .tint(.white)
+                    
+                    Button(action: { }) {
+                        Image(systemName: "delete.backward")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 20)
+                            .contentShape(.rect)
+                    }
+                    .tint(.white)
+                })
+                .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+        }
+        .padding()
+        .environment(\.colorScheme, .dark)
     }
     
     enum LockType: String {
